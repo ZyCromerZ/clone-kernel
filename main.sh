ac3c38a89e0d3fbf1d56bb5f0b28ecb3fb738cfe
ZyCromerZ <neetroid97@gmail.com>
2020-12-24 23:01:46 +0700

    clone different things :v
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- main.sh
+++ main.sh
@@ -1,7 +1,12 @@
 KERNEL=$(pwd)/kernel-dir
-git clone https://$fromKey@github.com/$fromUser/X01BD_Kernel -b 20201208/main $KERNEL
+if [ -z "$myKey" ];then
+    exit
+fi
+
+# git clone https://$fromKey@github.com/$fromUser/X01BD_Kernel -b 20201208/main $KERNEL
+git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_asus_X01BD.git -b ten $KERNEL
 cd $KERNEL
-git remote add myrepo https://$myKey@github.com/RyuujiX/kernel-x01bd
+git remote add myrepo https://$myKey@github.com/ZyCromerZ/vendor_asus_X01BD
 git push -f myrepo --all
 cd ..
 rm -rf $KERNEL
