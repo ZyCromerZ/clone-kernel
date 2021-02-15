4b3d080445edafd0e7d752403faf928ed8390f45
ZyCromerZ <neetroid97@gmail.com>
2021-02-15 10:10:07 +0700

    clone lancelot
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- main.sh
+++ main.sh
@@ -4,9 +4,9 @@ if [ -z "$myKey" ];then
 fi
 
 # git clone https://$fromKey@github.com/$fromUser/X01BD_Kernel -b 20201208/main $KERNEL
-git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_asus_X01BD.git -b ten $KERNEL
+git clone https://github.com/Redmi-MT6768/android_kernel_xiaomi_mt6768 -b eleven $KERNEL
 cd $KERNEL
-git remote add myrepo https://$myKey@github.com/ZyCromerZ/vendor_asus_X01BD
+git remote add myrepo https://$myKey@github.com/ZyCromerZ/lancelot
 git push -f myrepo --all
 cd ..
 rm -rf $KERNEL
