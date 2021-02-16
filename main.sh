25b1abfae70a25b8b0837b7db99de2d9d37fe60e
ZyCromerZ <neetroid97@gmail.com>
2021-02-16 13:53:20 +0700

    yaudah gas ae dah
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- main.sh
+++ main.sh
@@ -2,11 +2,17 @@ KERNEL=$(pwd)/kernel-dir
 if [ -z "$myKey" ];then
     exit
 fi
-
+addBranch()
+{
+    git fetch origin $1
+    git checkout FETCH_HEAD && git checkout -b $1
+}
 # git clone https://$fromKey@github.com/$fromUser/X01BD_Kernel -b 20201208/main $KERNEL
-git clone https://github.com/Redmi-MT6768/android_kernel_xiaomi_mt6768 -b eleven $KERNEL
+git clone https://github.com/ZyCromerZ/lancelot -b eleven $KERNEL
 cd $KERNEL
-git remote add myrepo https://$myKey@github.com/ZyCromerZ/lancelot
+git remote add myrepo https://$myKey@github.com/ZyCromerZ/lancelot_kernels
+addBranch eleven-upstream
+addBranch eleven-upstream-test
 git push -f myrepo --all
 cd ..
 rm -rf $KERNEL
